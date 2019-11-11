dbba03d1558915ad92c8185064e72a4ceadd7d46
Akshayrp <akshaypatwari24@gmail.com>
2019-11-11 16:17:56 +0530

    computation1(a+b*c)

---
+++ Computation.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+read -p "Enter first input:" input1
+read -p "Enter secound input:" input2
+read -p "Enter third input:" input3
+
+computation1=$(( $input1 + $input2 * $input3))
+
+echo "Answer of computation1 is : " $computation1	
