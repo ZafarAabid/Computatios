cd2c264f609f1959f67c5dd02d09b0415eee96b4
Akshayrp <akshaypatwari24@gmail.com>
2019-11-11 16:24:52 +0530

    computation2(a*b+c)

--- Computation.sh
+++ Computation.sh
@@ -6,4 +6,9 @@ read -p "Enter third input:" input3
 
 computation1=$(( $input1 + $input2 * $input3))
 
-echo "Answer of computation1 is : " $computation1	
+echo "Answer of computation1(input1+input2*input3) is : " $computation1	
+
+computation2=$(( $input1 * $input2 + $input3))
+
+echo "Answer of computation1(input1*input2+input3) is : " $computation2	
+
