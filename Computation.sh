3d731abcf17025f8ba0e0e3ccf43552eb33cc0f2
Akshayrp <akshaypatwari24@gmail.com>
2019-11-11 18:17:59 +0530

    dictionary+array added

--- Computation.sh
+++ Computation.sh
@@ -1,35 +1,57 @@
-#! /bin/bash
+#! /bin/bash -x
 
 read -p "Enter first input:" input1
 read -p "Enter secound input:" input2
 read -p "Enter third input:" input3
 
+declare -A computations
+counter=0
 
 function computation1() {
-	result=$(( $1 + $2 * $3))
-	echo $result
-}
+	result=$(( $1 + $2 * $3 ))
+	
+	computations["$((counter++))"]="$(( $1 + $2 * $3 ))"
+	results[$counter]="$(( $1 + $2 * $3 ))"
 
+}
 computation1 $input1 $input2 $input3
 
-function computation2() {
-	result=$(( $1 * $2 + $3))
-	echo $result
-}
 
+function computation2() {
+	result=$(( $1 * $2 + $3 ))
+	
+	computations["$((counter++))"]="$(( $1 * $2 + $3 ))"
+	results[$counter]="$(( $1 * $2 + $3 ))"
+	
+}
 computation2 $input1 $input2 $input3
 
-function computation3() {
-	result=$(( $1 + $2 / 3))
-	echo $result
-	}
 
+function computation3() {
+	result=$(( $1 + $2 / $3 ))
+	
+	computations["$((counter++))"]=$(( $1 + $2 / $3 ))
+	results[$counter]=$(( $1 + $2 / $3 ))
+
+	}
 computation3 $input1 $input2 $input3
 
 
-function computation4() {
-	result=$(( $1 % $2 + 3))
-	echo $result
-	}
 
+function computation4() {
+	result=$(( $1 % $2 + $3 ))
+	
+	computations["$((counter++))"]="$(( $1 % $2 + $3 ))"
+	results[$counter]="$(( $1 % $2 + $3 ))"
+
+	}
 computation4 $input1 $input2 $input3
+
+
+
+echo  "Array is "${results[@]}
+echo "dict is "${computations[@]}
+for (( counts=0; counts <= ${#computations} ; counts++ ))
+do
+	echo "${computations[$counts]}"
+done
