b1a349397f4bcbdb996239717dd3f3b3d6375cb2
Akshayrp <akshaypatwari24@gmail.com>
2019-11-11 16:34:47 +0530

    computation2(a*b+c)

--- Computation.sh
+++ Computation.sh
@@ -4,11 +4,16 @@ read -p "Enter first input:" input1
 read -p "Enter secound input:" input2
 read -p "Enter third input:" input3
 
-computation1=$(( $input1 + $input2 * $input3))
 
-echo "Answer of computation1(input1+input2*input3) is : " $computation1	
+function computation1() {
+	echo $(( $input1 + $input2 * $input3))
+}
 
-computation2=$(( $input1 * $input2 + $input3))
+echo "Answer of computation1(input1+input2*input3) is : " "$( compution1 $input1 $input2 $input3 )"	
 
-echo "Answer of computation1(input1*input2+input3) is : " $computation2	
+function computation2() {
+	echo $(( $input1 * $input2 + $input3))
+}
+
+echo "Answer of computation1(input1*input2+input3) is : " "$( compution2 $input1 $input2 $input3 )"	
 
